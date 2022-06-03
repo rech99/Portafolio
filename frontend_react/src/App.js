f183f2524ec7cfe3c3cf574c30102517222e60d7
Rech99 <rech_99@hotmail.com>
2022-06-03 14:27:22 -0700

    primero

---
+++ frontend_react/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { About, Footer, Header, Skills, Testimonials, Works } from './contanier';
+import {Navbar} from './components';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <Header />
+      <About />
+      <Works />
+      <Skills />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
